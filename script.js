35a93a6cb4077811ff574df56db1f7f490e68f56
DIYATHOKOZA <diyathokoza@gmail.com>
2024-08-27 11:46:05 +0200

    update

--- script.js
+++ script.js
@@ -55,3 +55,11 @@ function validateEmail(email) {
     const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
     return re.test(String(email).toLowerCase());
 }
+document.addEventListener('DOMContentLoaded', function () {
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    navToggle.addEventListener('click', function () {
+        navMenu.classList.toggle('active');
+    });
+});
